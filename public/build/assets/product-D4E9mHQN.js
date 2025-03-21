5a14cbad3f63181a8bd7a6e64c91bf6bcbc70fce
PunitDev0 <punitdeveloper1@gmail.com>
2025-03-21 11:13:35 +0530

    update urls

--- public/build/assets/product-D4E9mHQN.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"./app-ddHeRjmF.js";const e="http://127.0.0.1:8000/api/products",s="http://127.0.0.1:8000/api/categories",c={getAllProducts:async()=>t.get(e),getCategories:async()=>t.get(s),getProductById:async o=>{try{return(await t.get(`${e}/${o}`)).data}catch(r){throw console.error(`Error fetching order ${orderId}:`,r),r}}};export{c as p};
