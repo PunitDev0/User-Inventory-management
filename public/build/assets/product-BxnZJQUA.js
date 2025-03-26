fd1b3973d24337140b418ac472f73a3ee33918e1
PunitDev0 <punitdeveloper1@gmail.com>
2025-03-26 18:17:53 +0530

    update error

--- public/build/assets/product-BxnZJQUA.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"./app-BsADHLCK.js";const e="https://crm.aryanevents.com/user/public/api/products",s="https://crm.aryanevents.com/user/public/api/categories",a={getAllProducts:async()=>t.get(e),getCategories:async()=>t.get(s),getProductById:async o=>{try{return(await t.get(`${e}/${o}`)).data}catch(r){throw console.error(`Error fetching order ${orderId}:`,r),r}}};export{a as p};
